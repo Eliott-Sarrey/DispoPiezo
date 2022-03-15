f5bafc8ed9ba2a35b6ff15e91c7f252ffe2f6627
Eliott-Sarrey <eliott.sarrey@gmail.com>
2022-03-15 10:20:44 +0100

    Add tables

---
+++ installation.sh
@@ -0,0 +1,6 @@
+sudo pip3 install pyvisa pyvisa-py
+sudp pip3 install tikzplotlib
+sudo pip3 install ipywidgets
+sudo pip3 install scipy
+sudo pip3 install numpy
+conda install -c conda-forge ipypublish
